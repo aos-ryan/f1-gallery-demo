2f08e5caa958f98c22f2c403b0769a62cc80534c
ryan <ryan@arrayofstars.com>
2023-08-25 14:08:09 -0400

    remove directional light

--- components/DesktopModel.vue
+++ components/DesktopModel.vue
@@ -62,7 +62,7 @@ export default {
       const ambientLight = new THREE.AmbientLight(0xffffff, 1)
       scene.add(ambientLight)
 
-      const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8)
+      // const directionalLight = new THREE.DirectionalLight(0xffffff, 0.8)
       // directionalLight.castShadow = true
       // directionalLight.shadow.mapSize.set(1024, 1024)
       // directionalLight.shadow.camera.far = 15
@@ -70,8 +70,8 @@ export default {
       // directionalLight.shadow.camera.top = 7
       // directionalLight.shadow.camera.right = 7
       // directionalLight.shadow.camera.bottom = -7
-      directionalLight.position.set(0, 5, 0)
-      scene.add(directionalLight)
+      // directionalLight.position.set(0, 5, 0)
+      // scene.add(directionalLight)
 
       const spotLight = new THREE.SpotLight(0xffffff)
       spotLight.position.set(0, 2, 1)
